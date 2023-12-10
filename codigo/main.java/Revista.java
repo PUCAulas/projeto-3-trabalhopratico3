612e9d3a28af1c4cbb8cb4ec7ebf7b7bfb31a4ce
Maria Eduarda <104326131+dudachrispim@users.noreply.github.com>
2023-12-10 12:38:22 -0300

    Update Revista.java

--- codigo/main.java/Revista.java
+++ codigo/main.java/Revista.java
@@ -1,5 +1,8 @@
 package main.java;
 
+import java.util.HashSet;
+import java.util.Set;
+
 //Classe para revistas
 public class Revista extends ItemBiblioteca {
     public Revista(String titulo, String autor, int anoPublicacao, boolean emprestavel, int quantidadeExemplares) {
@@ -36,5 +39,14 @@ public class Revista extends ItemBiblioteca {
     @Override
     public String toString() {
         return "Revista - " + super.toString() + ", Emprestável: " + emprestavel + ", Quantidade de Exemplares: " + quantidadeExemplares;
+    }
+    
+ // Implementação do método abstrato getCategorias para Revista
+    @Override
+    public Set<String> getCategorias() {
+        // Lógica para obter categorias específicas de Revista
+        Set<String> categorias = new HashSet<>();
+        categorias.add("Periódico"); 
+        return categorias;
     }
 }
